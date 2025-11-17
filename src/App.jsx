250c5eb19d215eab286c6846a46ce1b42ab5ffba
abdaLLa230 <abdalla7ali@icloud.com>
2025-11-17 02:16:58 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ import MindMaps from './Components/MindMaps/MindMaps'
 let x = createBrowserRouter([
   {path:"" , element : <Layout/> ,
   children : [
-    { index : true  , element : <MindMaps/>},
+    { index : true  , element : <Home/>},
     {path:"Chapter" , element : <Chapters/>},
     {path:"Creative", element : <CreativeThinking/>},
     {path:"Feed"    , element : <Feedback/>},
-    // {path:"Mindmap" , element : <MindMaps/>},
+    {path:"Mindmap" , element : <MindMaps/>},
     {path:"*"       , element : <Notfound/>},
 
   
